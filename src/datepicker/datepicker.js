9f42d8b6cf36e33f4e9c128958eeb662b9fd02b6
wuranbo <wu.ranbo@yottabyte.cn>
2014-11-21 15:53:42 +0800

    fix(datepicker): not auto focus the popup.

--- src/datepicker/datepicker.js
+++ src/datepicker/datepicker.js
@@ -564,7 +564,7 @@ function ($compile, $parse, $document, $position, dateFilter, dateParser, datepi
 
       scope.$watch('isOpen', function(value) {
         if (value) {
-          scope.$broadcast('datepicker.focus');
+          // scope.$broadcast('datepicker.focus'); // wu.ranbo not autofocus the popup
           scope.position = appendToBody ? $position.offset(element) : $position.position(element);
           scope.position.top = scope.position.top + element.prop('offsetHeight');
 
